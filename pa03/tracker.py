9758b80cd2efd4475e349dadab5d2eeac1eb741d
dexinhuang <126409255+dexinhuang@users.noreply.github.com>
2023-03-26 23:15:25 -0400

    Tracker and transaction optimizations

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -1,73 +1,175 @@
-import sys
-from transactions import Transaction
-
-
-def print_menu():
-    """Print the menu of available commands."""
-    menu = """
-=======================================
-           TRANSACTION MENU
-  Type any of the following commands.
-
-  1. show_categories
-  2. add_category <category>
-  3. modify_category <category> <modified category name>
-  4. show_transactions
-  5. add_transaction <category> <trans. amount> <date (YYYY-MM-DD)> <description>
-  6. delete_transaction <transaction id>
-  7. summarize_transactions_date
-  8. summarize_transactions_month
-  9. summarize_transactions_year
-  10. summarize_transactions_category
-  11. help
-  12. reselect_file <file_path>
-  13. quit
-
-=======================================
 """
-    print(menu)
+This program is written by Angela Zhang and Dexin Huang
+
+Tracker is an app that maintains a transactions database.
+
+It also uses an Object Relational Mapping (ORM)
+to abstract out the database operations from the
+UI/UX code.
+
+The ORM, Transaction, will map SQL rows with the schema
+    (rowid, amount, category, date, description)
+to Python Dictionaries as follows:
+
+(100, 'food', 2023-10-5, 'beef') <-->
+{rowid:1,
+ amount:100,
+ category:'food',
+ date:'2023-10-5',
+ description:'beef'
+}
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/transactions.db
+"""
+import sys
+import datetime
+from transaction import Transaction
 
 
-def main():
-    """Main function to handle user input and execute commands."""
-    print("\nEnter your database filename. If the file does not exist, a new one will be created.")
-    filename = input("filename >>> ")
-    transaction = Transaction(filename)
-    print_menu()
+def print_usage():
+    ''' print the menu options '''
+    print('''usage:
+            0. quit
+            1. show transactions
+            2. add transaction
+            3. delete transaction
+            4. summarize transactions by date
+            5. summarize transactions by month
+            6. summarize transactions by year
+            7. summarize transactions by category
+            8. show menu
+            '''
+          )
 
-    commands = {
-        "show_categories": transaction.get_categories,
-        "add_category": transaction.add_category,
-        "modify_category": transaction.modify_category,
-        "show_transactions": transaction.get_transactions,
-        "add_transaction": transaction.add_transaction,
-        "delete_transaction": transaction.delete_transaction,
-        "summarize_transactions_date": transaction.summarize_by_date,
-        "summarize_transactions_month": transaction.summarize_by_month,
-        "summarize_transactions_year": transaction.summarize_by_year,
-        "summarize_transactions_category": transaction.summarize_by_category,
-    }
 
-    while True:
-        command_input = input(">>> ")
-        if command_input.strip() == "help":
-            print_menu()
-        elif command_input.strip() == "quit":
-            break
-        else:
-            command_parts = command_input.split(" ")
-            command = command_parts[0]
-            args = command_parts[1:]
+def print_summary(header, transaction):
+    '''
+    Print a summary of transactions based on the provided header.
 
-            if command in commands:
-                try:
-                    result = commands[command](*args)
-                    if result is not None:
-                        print(result)
-                except TypeError:
-                    print(f"Invalid number of arguments for command '{command}'. Type 'help' for usage information.")
+    Args:
+        header (str): A formatted string representing the header of the summary table.
+        transaction (list): A list of transaction dictionaries.
+
+    Note:
+        If the transaction list is empty, it will print 'no tasks to print'.
+    '''
+    if len(transaction) == 0:
+        print('no tasks to print')
+        return
+
+    print('\n' + header)
+    print('-' * len(header))
+
+    for item in transaction:
+        values = tuple(item.values())
+        print(f"{values[0]:<18} {values[1]}")
+
+
+def print_transactions(transaction):
+    '''
+    Print a formatted table of transactions with headers and values.
+
+    Args:
+        transaction (list): A list of transaction dictionaries.
+
+    Note:
+        If the transaction list is empty, it will print 'no tasks to print'.
+    '''
+    if len(transaction) == 0:
+        print('no tasks to print')
+        return
+
+    print('\n' + f"{'item #':<10} {'amount':<12} {'category':<15} {'date':<18} {'description'}")
+    print('-' * 75)
+    for item in transaction:
+        values = tuple(item.values())
+        print(f"{values[0]:<10} {values[1]:<12} {values[2]:<15} {values[3]:<18} {values[4]}")
+
+
+def process_args(arglist):
+    '''
+    Process the command line arguments and execute the corresponding transaction operations.
+
+    Args:
+        arglist (list): A list of command line arguments.
+
+    Note:
+        Supported commands include: quit, show transactions, add, delete, summarize by date,
+        summarize by month, summarize by year, summarize by category, and show menu.
+        If an unsupported command is entered, it will print "is not implemented" and show the usage.
+    '''
+    transaction = Transaction("transactions.db")
+
+    if not arglist:
+        print_usage()
+    elif 'quit' in arglist[0]:
+        print('\n' + "You have been disconnected" + '\n')
+        sys.exit()
+    elif 'show transactions' in arglist[0]:
+        print_transactions(transaction.show_transactions())
+    elif 'add' in arglist[0]:
+        amount = int(input('\n' + 'Enter amount: '))
+        category = input('Enter category: ')
+        date = datetime.datetime.strptime(input('Enter date (yyyy-mm-dd): '), '%Y-%m-%d')
+        description = input('Enter description: ')
+        info = {'amount': amount, 'category': category, 'date': date.date(), 'description': description}
+        transaction.add_transaction(info)
+    elif 'delete' in arglist[0]:
+        print_transactions(transaction.show_transactions())
+        print('-' * 75 + '\n')
+        num = input('Enter transaction id: ')
+        print_transactions(transaction.delete_transaction(num))
+    elif 'summarize by' in arglist[0]:
+        if 'date' in arglist[0]:
+            print_summary("date         amount", transaction.summarize_by_date())
+        elif 'month' in arglist[0]:
+            print_summary("month        amount", transaction.summarize_by_month())
+        elif 'year' in arglist[0]:
+            print_summary("year         amount", transaction.summarize_by_year())
+        elif 'category' in arglist[0]:
+            print_transactions(transaction.summarize_by_category())
+    elif 'menu' in arglist[0]:
+        print_usage()
+    else:
+        print(arglist, "is not implemented")
+        print_usage()
+
+
+def toplevel():
+    '''
+    Process command line arguments and execute corresponding transaction operations.
+
+    Note:
+        If no arguments are passed, the function will enter an interactive command prompt mode,
+        continuously asking for user input until an empty input is received.
+        In both cases, whether arguments are passed or not, the function will call process_args()
+        to handle the entered commands and execute the corresponding operations.
+    '''
+    if len(sys.argv) == 1:
+        # they didn't pass any arguments,
+        # so prompt for them in a loop
+        print('\n' + '-' * 40)
+        print_usage()
+        args = []
+        while args != ['']:
+            args = input("command> ").split(' ')
+            curr = args[0]
+            args = [' '.join(args[0:])]
+            process_args(args)
+            if curr == 'summarize':
+                print('-' * 30 + '\n')
+            elif args[0] == 'show transactions':
+                print('-' * 75 + '\n')
             else:
-                print("Unknown command. Type 'help' for a list of available commands.")
+                print('-' * 40 + '\n')
+    else:
+        # read the args and process them
+        args = sys.argv[1:]
+        process_args(args)
+        print('-' * 40 + '\n' * 3)
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    toplevel()
--- pa03/transaction.py
+++ pa03/transaction.py
@@ -1,101 +1,116 @@
-"""
-Transaction module for managing transactions
-"""
+'''
+This program is written by Angela Zhang and Dexin Huang
+
+Transaction is an Object Relational Mapping to the transactions.db database
+
+The ORM will work map SQL rows with the schema
+    (rowid, amount, category, date, description)
+to Python Dictionaries as follows:
+
+(100, 'food', 2023-10-5, 'beef') <-->
+{rowid:1, amount:100, category:'food', date:'2023-10-5', description:'beef'}
+
+In place of SQL queries, we will have method calls.
+
+This app will store the data in a SQLite database ~/transactions.db
+
+'''
 import sqlite3
 
+def to_dict(tuples):
+    '''
+    Convert a tuple of transaction information into a dictionary.
+
+    Args:
+        tuples: A tuple containing transaction information in the order
+                (rowid, amount, category, date, description)
+
+    Returns:
+        A dictionary containing the transaction information with keys
+        'rowid', 'amount', 'category', 'date', and 'description'.
+
+    Note:
+        The input tuple can have less than 5 elements, and the returned
+        dictionary will only have keys for the available elements.
+    '''
+    transaction = {}
+    if len(tuples) >= 1:
+        transaction['rowid'] = tuples[0]
+    if len(tuples) >= 2:
+        transaction['amount'] = tuples[1]
+    if len(tuples) >= 3:
+        transaction['category'] = tuples[2]
+    if len(tuples) >= 4:
+        transaction['date'] = tuples[3]
+    if len(tuples) >= 5:
+        transaction['description'] = tuples[4]
+    return transaction
+
+
+def tuples_to_dicts(t_s):
+    '''
+    Convert a list of transaction tuples to a list of dictionaries.
+
+    Args:
+        t_s: A list of tuples, where each tuple contains transaction information
+             in the order (rowid, amount, category, date, description)
+
+    Returns:
+        A list of dictionaries, where each dictionary contains the transaction
+        information with keys 'rowid', 'amount', 'category', 'date', and 'description'.
+
+    Note:
+        This function utilizes the to_dict() function for converting individual tuples.
+    '''
+    return [to_dict(tuples) for tuples in t_s]
+
+
 class Transaction:
-    """
-    Transaction class for handling transaction operations.
-    """
-
+    '''Initialize the class and create the table if there has no such table'''
     def __init__(self, filename):
-        self.conn = sqlite3.connect(filename)
-        self.create_table()
+        self.filename = filename
+        self.run_query('''CREATE TABLE IF NOT EXISTS transactions
+                    (amount int, category text, date int, description text)''',())
 
-    def create_table(self):
-        cursor = self.conn.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS transactions (
-                        id INTEGER PRIMARY KEY,
-                        date TEXT,
-                        description TEXT,
-                        category TEXT,
-                        amount REAL
-                        )""")
-        self.conn.commit()
+    def show_transactions(self):
+        ''' Show all the transactions '''
+        return self.run_query("SELECT rowid, * FROM transactions",())
 
-    def add_transaction(self, amount, category, date, description):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)", (
-                amount, category, date, description))
-        self.conn.commit()
+    def add_transaction(self, item):
+        ''' Add a new transaction '''
+        return self.run_query("INSERT INTO transactions VALUES(?,?,?,?)",
+                              (item['amount'], item['category'], item['date'], item['description']))
 
-    def delete_transaction(self, transaction_id):
-        cursor = self.conn.cursor()
-        cursor.execute("DELETE FROM transactions WHERE id=?", (transaction_id,))
-        self.conn.commit()
-
-    def update_transaction(self, transaction_id,
-                           amount=None, category=None, date=None, description=None):
-        cursor = self.conn.cursor()
-        updates = {}
-        if amount:
-            updates['amount'] = amount
-        if category:
-            updates['category'] = category
-        if date:
-            updates['date'] = date
-        if description:
-            updates['description'] = description
-        query = "UPDATE transactions SET " + ", ".join(
-            f"{key}=?" for key in updates.keys()) + " WHERE id=?"
-        params = tuple(updates.values()) + (transaction_id,)
-        cursor.execute(query, params)
-        self.conn.commit()
-
-    def get_transactions(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM transactions")
-        return cursor.fetchall()
-
-    def get_categories(self):
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT DISTINCT category FROM transactions")
-        return [row[0] for row in cursor.fetchall()]
+    def delete_transaction(self, rowid):
+        ''' Delete a transaction by item_no '''
+        return self.run_query("DELETE FROM transactions WHERE rowid=?", (rowid,))
 
     def summarize_by_date(self):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "SELECT date, SUM(amount) FROM transactions GROUP BY date")
-        return cursor.fetchall()
+        ''' Summarize transactions by date '''
+        return self.run_query("SELECT date, SUM(amount) FROM transactions GROUP BY date",())
 
     def summarize_by_month(self):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "SELECT strftime('%Y-%m', date) AS month, SUM(amount) FROM transactions GROUP BY month")
-        return cursor.fetchall()
+        ''' Summarize transactions by month '''
+        return self.run_query(
+            "SELECT strftime('%m', date) AS month, SUM(amount) FROM transactions GROUP BY month",())
 
     def summarize_by_year(self):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year")
-        return cursor.fetchall()
+        ''' Summarize transactions by year '''
+        return self.run_query(
+            "SELECT strftime('%Y', date) AS year, SUM(amount) FROM transactions GROUP BY year",())
 
     def summarize_by_category(self):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "SELECT category, SUM(amount) FROM transactions GROUP BY category")
-        return cursor.fetchall()
+        ''' Summarize transactions by category '''
+        return self.run_query(
+            "SELECT category, SUM(amount) AS amount FROM transactions GROUP BY category",())
 
-    def add_category(self, category):
-        cursor = self.conn.cursor()
-        cursor.execute(
-            "INSERT INTO transactions (category) VALUES (?)", (
-                category,))
-        self.conn.commit()
-
-    def modify_category(self, old_category, new_category):
-        cursor = self.conn.cursor()
-        cursor.execute("UPDATE transactions SET category=? WHERE category=?", (
-            new_category, old_category))
-        self.conn.commit()
+    def run_query(self, query, data):
+        ''' Execute a SQL query and return the results '''
+        # con = sqlite3.connect(os.getenv('HOME') + '/' + self.filename)
+        con = sqlite3.connect(self.filename)
+        cur = con.cursor()
+        cur.execute(query,data)
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return tuples_to_dicts(tuples)
